636f030cee060396526202d409a8f224c377b475
fvalladaresj <fvalladares.j@gmail.com>
2022-06-25 04:59:50 -0400

    create edit and delete contacts

---
+++ src/useForm.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+
+export interface IForm {
+    name: string;
+    lastname1: string;
+    lastname2: string;
+    email: string;
+    phone: string;
+}
+
+const useForm = ({ initState, callback, validator }: any) => {
+  const [state, setState] = useState(initState);
+  const [errors, setErrors] = useState({
+    name: "",
+    lastname1: "",
+    lastname2: "",
+    email: "",
+    phone: "",
+  });
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  useEffect(() => {
+    const isValidErrors = () =>
+      Object.values(errors).filter(error => error !== "")
+        .length > 0;
+    if (isSubmited && !isValidErrors()) callback();
+  }, [errors]);
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setState(() => ({
+      ...state,
+      [name]: value
+    }));
+  };
+
+  const handleBlur = (e: any) => {
+    const { name: fieldName } = e.target;
+    const failFields = validator(state, fieldName);
+    setErrors(() => ({
+      ...errors,
+      [fieldName]: Object.values(failFields)[0]
+    }));
+  };
+
+  const handleSubmit = (e: any) => {
+    console.log("Submitted from form")
+    e.preventDefault();
+    const { name: fieldName } = e.target;
+    const failFields = validator(state, fieldName);
+    setErrors(() => ({
+      ...errors,
+      [fieldName]: Object.values(failFields)[0]
+    }));
+    setIsSubmited(true);
+  };
+
+  return {
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    state,
+    errors
+  };
+};
+
+export default useForm;
