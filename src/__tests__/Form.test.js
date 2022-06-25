a73ce8181eac8e42ded66ea833f95ef8a07f720f
fvalladaresj <fvalladares.j@gmail.com>
2022-06-25 18:01:38 -0400

    form tests

--- src/__tests__/Form.test.js
+++ src/__tests__/Form.test.js
@@ -2,19 +2,11 @@ import React from "react";
 import App from "../App";
 import Enzyme, { mount, render, shallow, ReactWrapper } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { TextField } from "@mui/material";
+import { validator } from "../Validator";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const simulateChangeOnInput = (wrapper, inputSelector, newValue) => {
-  const input = wrapper.find("Form").dive().find(inputSelector);
-  input.simulate("change", {
-    target: { value: newValue },
-  });
-  return wrapper.find("Form").dive().find(inputSelector);
-};
-
-describe("Form", () => {
+describe("Pruebas", () => {
   it("Formulario añadir contacto vacío", () => {
     const wrapper = shallow(<App />);
     const addContactBtn = wrapper.find("#addContactBtn");
@@ -128,4 +120,57 @@ describe("Form", () => {
     }
   });
 
+  it("Validar campos del formulario", () => {
+    const defaultState = {
+      name: "",
+      lastname1: "",
+      lastname2: "",
+      email: "",
+      phone: "",
+    };
+    const validatorResult = (fieldName, value) => {
+      let errors = validator(
+        {
+          ...defaultState,
+          [fieldName]: value,
+        },
+        fieldName
+      );
+      return errors[fieldName] ? errors[fieldName] == "" : true;
+    };
+    //Nombres
+    expect(validatorResult("name", "")).toBe(false);
+    expect(validatorResult("name", "Juan")).toBe(true);
+    expect(validatorResult("name", "juan")).toBe(true);
+    expect(validatorResult("name", "123")).toBe(false); //números
+    expect(validatorResult("name", "juan123")).toBe(false); //letras y números
+    expect(validatorResult("name", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false); //101 caracteres
+    
+    //Apellidos
+    expect(validatorResult("lastname1", "")).toBe(false);
+    expect(validatorResult("lastname1", "Juan")).toBe(true);
+    expect(validatorResult("lastname1", "juan")).toBe(true);
+    expect(validatorResult("lastname1", "123")).toBe(false); //números
+    expect(validatorResult("lastname1", "juan123")).toBe(false); //letras y números
+    expect(validatorResult("lastname1", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false); //101 caracteres
+    expect(validatorResult("lastname2", "")).toBe(false);
+    expect(validatorResult("lastname2", "Juan")).toBe(true);
+    expect(validatorResult("lastname2", "juan")).toBe(true);
+    expect(validatorResult("lastname2", "123")).toBe(false); //números
+    expect(validatorResult("lastname2", "juan123")).toBe(false); //letras y números
+    expect(validatorResult("lastname2", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false); //101 caracteres
+  
+    //Email
+    expect(validatorResult("email", "")).toBe(false);
+    expect(validatorResult("email", "Juan")).toBe(false);
+    expect(validatorResult("email", "Juan@")).toBe(false);
+    expect(validatorResult("email", "Juan@gmail")).toBe(false);    
+    expect(validatorResult("email", "Juan@gmail.com")).toBe(true);
+    
+    //Telefono
+    expect(validatorResult("phone", "")).toBe(false);
+    expect(validatorResult("phone", "1")).toBe(false);
+    expect(validatorResult("phone", "12345678")).toBe(true);
+    expect(validatorResult("phone", "123456789")).toBe(false);
+  });
 });
